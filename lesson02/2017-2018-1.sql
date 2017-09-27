f88a8d9b17b0aff8ed0e069110c76d4e5672d2d5
alexaegis <alexaegis@gmail.com>
2017-09-27 10:26:54 +0200

    a

---
+++ lesson02/2017-2018-1.sql
@@ -0,0 +1,25 @@
+---SZERET---
+------------
+
+--Melyek azok a gyümölcsök, amelyeket Micimackó szeret?
+--Melyek azok a gyümölcsök, amelyeket Micimackó nem szeret? (de valaki más igen)
+--Kik szeretik az almát?
+--Kik nem szeretik a körtét? (de valami mást igen)
+--Kik szeretik vagy az almát vagy a körtét?
+--Kik szeretik az almát is és a körtét is?
+--Kik azok, akik szeretik az almát, de nem szeretik a körtét?
+
+
+---DOLGOZO---
+-------------
+
+--Kik azok a dolgozók, akiknek a fizetése nagyobb, mint 2800?
+--Kik azok a dolgozók, akik a 10-es vagy a 20-as osztályon dolgoznak?
+--Kik azok, akiknek a jutaléka nagyobb, mint 600?
+--Kik azok, akiknek a jutaléka nem nagyobb, mint 600?
+--Kik azok a dolgozók, akiknek a jutaléka ismeretlen (nincs kitöltve, vagyis NULL)?
+--Adjuk meg a dolgozók között elõforduló foglalkozások neveit.
+--Adjuk meg azoknak a nevét és kétszeres fizetését, akik a 10-es osztályon dolgoznak.
+--Kik azok a dolgozók, akik 1982.01.01 után léptek be a céghez?
+--Kik azok, akiknek nincs fõnöke?
+--Kik azok a dolgozók, akiknek a fõnöke KING? (egyelõre leolvasva a képernyõrõl) 
