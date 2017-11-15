0dc6ad3763c14cfc5ef8baadc093ef40263ff7c0
Győri Sándor <alexaegis@INF.ELTE.HU>
2017-11-15 11:53:23 +0100

    zh2

--- lesson08 - zh/zh2.sql
+++ lesson08 - zh/zh2.sql
@@ -55,6 +55,102 @@ FROM DOLGOZO
 WHERE SUBSTR(FOGLALKOZAS,1,1) = 'C' OR SUBSTR(FOGLALKOZAS,1,1) = 'M'
 ORDER BY FOGLALKOZAS;
 
---3
-SELECT
+/*
+DNEV       NEV_LENGHT
+---------- ----------
+JAMES               5
+MILLER              6
+ADAMS               5
+LOLA                4
+SMITH               5
+JONES               5
+CLARK               5
+BLAKE               5
+
+*/
+
+--3)
+SELECT AVG(COALESCE(JUTALEK, 0)) AS AVG_FIZ FROM DOLGOZO;
+
+/*
+   AVG_FIZ
+----------
+156,25    
+*/
+
+--4)
+SELECT KATEGORIA, COUNT(DNEV)
+FROM DOLGOZO JOIN FIZ_KATEGORIA ON FIZETES BETWEEN ALSO AND FELSO 
+GROUP BY KATEGORIA
+ORDER BY KATEGORIA;
+
+/* 
+ KATEGORIA COUNT(DNEV)
+---------- -----------
+         1           4
+         2           3
+         3           3
+         4           5
+         5           1
+
+*/
+
+--5)
+SELECT DKOD, DNEV, FOGLALKOZAS, JUTALEK, BELEPES--, FIZETES, OAZON 
+FROM DOLGOZO 
+WHERE OAZON IS NOT NULL AND FIZETES IN (
+    SELECT --DNEV, FOGLALKOZAS, JUTALEK, BELEPES, 
+    MIN(FIZETES)
+    FROM DOLGOZO NATURAL JOIN OSZTALY
+    GROUP BY OAZON);
+    
+/*
+      DKOD DNEV       FOGLALKOZ    JUTALEK BELEPES     
+---------- ---------- --------- ---------- ------------
+      7900 JAMES      CLERK                81-DEC.  -03
+      7369 SMITH      CLERK                80-DEC.  -17
+      7934 MILLER     CLERK                82-JAN.  -23
+*/
+    
+--6)
+SELECT SUM(COUNT(DISTINCT F.DNEV)) AS NON_MANAGER_BOSS 
+FROM DOLGOZO F JOIN DOLGOZO D ON F.DKOD = D.FONOKE 
+WHERE F.FOGLALKOZAS != 'MANAGER' GROUP BY F.FOGLALKOZAS;
+
+/*
+NON_MANAGER_BOSS
+----------------
+               3
+*/
+
+--7) Ez rossz lett :(
+SELECT NEV1, NEV2, SZ FROM (
+select SZ1.NEV AS NEV1, SZ2.NEV AS NEV2, 
+   case
+      when SZ1.GYUMOLCS = SZ2.GYUMOLCS then 'true' 
+      else 'false' 
+   end AS SZ
+from SZERET SZ1, SZERET SZ2) WHERE SZ = 'true' GROUP BY NEV1, NEV2, SZ HAVING COUNT(NEV2) = 2 AND COUNT(NEV1) = 2;
+
+/*
+NEV1            NEV2            SZ   
+--------------- --------------- -----
+Tigris          Tigris          true 
+Tigris          Kanga           true 
+Micimackó       Tigris          true 
+Malacka         Kanga           true 
+Micimackó       Kanga           true 
+Kanga           Malacka         true 
+Kanga           Micimackó       true 
+Tigris          Micimackó       true 
+Malacka         Tigris          true 
+Kanga           Kanga           true 
+Kanga           Tigris          true 
+
+NEV1            NEV2            SZ   
+--------------- --------------- -----
+Tigris          Malacka         true 
+
+12 rows selected. 
+*/
 
